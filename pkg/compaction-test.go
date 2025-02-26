57ac95e0db3e5269aee9a2e5ea0663f5553b03e9
Scott Dodson <sdodson@redhat.com>
2025-02-26 13:19:33 -0500

    Add a compaction test

---
+++ pkg/compaction-test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "time"
+
+    clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+func main() {
+    client, err := clientv3.New(clientv3.Config{
+        Endpoints: []string{"http://localhost:2400"},
+    })
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer client.Close()
+
+    ctx := context.Background()
+
+    // Generate 10,000 unique keys with 10,000 revisions each
+    for i := 0; i < 1000; i++ {
+        key := fmt.Sprintf("key%d", i)
+        for j := 0; j < 1000; j++ {
+            _, err := client.Put(ctx, key, fmt.Sprintf("value%d", j))
+            if err != nil {
+                log.Fatal(err)
+            }
+        }
+        if i%10 == 0 {
+            fmt.Printf("Generated %d keys\n", i)
+        }
+    }
+
+    // Get the current revision
+    status, err := client.Status(ctx, "http://localhost:2400")
+    if err != nil {
+        log.Fatal(err)
+    }
+    currentRevision := status.Header.Revision
+
+    // Perform compaction
+    start := time.Now()
+    _, err = client.Compact(ctx, currentRevision)
+    if err != nil {
+        log.Fatal(err)
+    }
+    duration := time.Since(start)
+
+    fmt.Printf("Compaction completed in %v\n", duration)
+
+    // Defragment to reclaim space
+    _, err = client.Defragment(ctx, "http://localhost:2400")
+    if err != nil {
+        log.Fatal(err)
+    }
+}
